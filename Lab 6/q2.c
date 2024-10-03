f28a0cea52f34f72b1601ebe5a2eb9f72095abaa
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-10-03 15:19:35 +0500

    Create q2.c

---
+++ Lab 6/q2.c
@@ -0,0 +1,13 @@
+//2. Write a program to check whether a given number is prime or not.
+#include <stdio.h>
+int main()
+{
+	int i, num;
+	char prime = 'n' 
+	printf("enter num: ");
+	scanf("%d", &num);
+	for (num > 1, num--)
+	{
+		
+	}
+}
