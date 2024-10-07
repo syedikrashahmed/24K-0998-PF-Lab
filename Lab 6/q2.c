f92f006c5a1ba2e275089aeb1e2133293664ca28
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-10-08 00:20:14 +0500

    Update q2.c

--- Lab 6/q2.c
+++ Lab 6/q2.c
@@ -2,12 +2,21 @@
 #include <stdio.h>
 int main()
 {
-	int i, num;
-	char prime = 'n' 
-	printf("enter num: ");
-	scanf("%d", &num);
-	for (num > 1, num--)
-	{
-		
-	}
+        int i, num;
+        char prime = 'n' ;
+        printf("enter num: ");
+        scanf("%d", &num);
+        for (i = num - 1 ; i > 1 ; i--)
+        {
+            if (num % i == 0)
+            { prime = 'y'; }
+        }
+        switch (prime)
+        {
+            case 'n': printf("is prime");
+            break;
+            case 'y' : printf("is not prime");
+            break;
+            default:;
+        }
 }
