858745d0bdaf3bba206d837c613eb2fe5abfce09
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-10-08 00:19:36 +0500

    Update q1.c

--- Lab 6/q1.c
+++ Lab 6/q1.c
@@ -1,16 +1,20 @@
+//1. Which loop system would be better for user input. Justify your answer by creating a  program thattakes a value and adds it to a variable and prints it repeatedly until the user enters a zero value.
+
+// while loop
+//e.g:
 #include <stdio.h>
 int main()
 {
-	int userinput, sum = 0;
-	printf("enter num: ");
-	scanf("%d", &userinput);
-	sum = sum + userinput;
-	printf("%d\n", sum);
-	while(userinput != 0)
-	{
-		printf("enter num: ");
-		scanf("%d", &userinput);
-		sum = sum + userinput;
-		printf("%d\n", sum);
-	}
+        int userinput, sum = 0;
+        printf("enter num: ");
+        scanf("%d", &userinput);
+        sum = sum + userinput;
+        printf("%d\n", sum);
+        while(userinput != 0)
+        {
+                printf("enter num: ");
+                scanf("%d", &userinput);
+                sum = sum + userinput;
+                printf("%d\n", sum);
+        }
 }
