927e8fa1d5050626473c2a58144a11a6ae950ee9
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-10-08 20:12:47 +0500

    Update Lab 6

---
+++ Lab 6/q6.c
@@ -0,0 +1,15 @@
+//6. Make an opposite of the above-mentioned pattern using do-while loop
+
+#include <stdio.h>
+int main()
+{
+
+        for (int i = 0; i < 6; i++)
+        {
+                if (i == 0 || i == 5)
+                        printf("*   *\n");
+                if (i > 0 && i < 5)
+                        
+                        printf("*****\n");
+        }
+}
