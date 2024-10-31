30560b74c532dcfd8f710a45fbaef6c35a14d2ea
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-10-31 11:56:17 +0500

    Update q1.c

--- Lab 8/q1.c
+++ Lab 8/q1.c
@@ -1,1 +1,29 @@
-..
+/*1. Write a C program that generates a sequence of prime numbers within a given range using
+nested loops.*/
+
+#include <stdio.h>
+int main()
+{
+    int lower, upper, i, j;
+    printf("Enter lower bound of sequence: ");
+    scanf("%d", &lower);
+    printf("Enter upper bound of sequence: ");
+    scanf("%d", &upper);
+
+    for (i = lower; i <= upper; i++)
+    {
+        int flag = 0;
+        for(j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            printf(" %d", i);
+        }
+    }
+}
