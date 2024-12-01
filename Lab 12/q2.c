e07c741befdc3b0a11497c33836ed754c06305cd
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-12-02 00:57:52 +0500

    Update Lab 12

---
+++ Lab 12/q2.c
@@ -0,0 +1,26 @@
+/*Task 2: Write a program to implement the exchange or swap the values of 3 variables{a,b,c}. Toimplement this, you need to write a function called swaped().
+void swaped(int *aPtr, int *bPtr, int *cPtr);
+such that;
+b ----> temp
+a ----> b
+c ----> a
+temp -> a*/
+#include <stdio.h>
+
+void swaped(int *aPtr, int *bPtr, int *cPtr);
+
+int main()
+{
+	int a = 1, b = 2, c = 3;
+	swaped(&a, &b, &c);
+	printf("a = %d\nb = %d\nc = %d", a, b, c);
+	
+}
+
+void swaped(int *aPtr, int *bPtr, int *cPtr)
+{
+    int temp= *bPtr;
+    *bPtr = *cPtr;
+    *cPtr = *aPtr;
+    *aPtr = temp;
+}
