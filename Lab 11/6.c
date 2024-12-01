ef145f6f3ebeeded1b9708b1de1b92ca92456d32
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2024-12-01 19:29:40 +0500

    Update 6.c

--- Lab 11/6.c
+++ Lab 11/6.c
@@ -1,4 +1,6 @@
-/*6. Define a structure Employee with fields name, id, salary, and a nested structure Address(fields: city and zip_code). Write a program to input details for multiple employees, storethem in a file, and read them back to display.*/
+/*6. Define a structure Employee with fields name, id, salary, and a nested structure Address
+(fields: city and zip_code). Write a program to input details for multiple employees, store
+them in a file, and read them back to display.*/
 #include <stdio.h>
 #include <string.h>
 
@@ -16,32 +18,27 @@ typedef struct
     Address address;
 }Employee;
 
-int main()
+int main ()
 {
     Employee employees[3];
     for(int i = 0; i<3; i++)
     {
         printf("\nEMPLOYEE %d INPUTS\n", i + 1);
-
         printf("Enter Name: ");
         getchar();
         gets(employees[i].name);
-
         printf("Enter ID: ");
         scanf("%d", &employees[i].id);
-
         printf("Enter Salary: ");
         scanf("%f", &employees[i].salary);
-
         printf("Enter City: ");
         getchar();
         gets(employees[i].address.city);
-
         printf("Enter Zip Code: ");
         scanf("%d", &employees[i].address.zipcode);
     }
     
-    FILE* fptr=fopen("employee.txt", "w");
+    FILE* fptr=fopen("employee.txt", "wb");
     if (fptr == NULL) 
     {
         printf("File not found");
@@ -52,4 +49,26 @@ int main()
          fwrite(employees, sizeof(Employee), 1, fptr);
     }   
     fclose(fptr);
+    
+    fptr=fopen("employee.txt", "rb");
+    if (fptr == NULL) 
+    {
+        printf("File not found");
+        return 0;
+    }
+    
+    Employee readEmployees[3];   
+    fread(readEmployees, sizeof(Employee), 3, fptr);
+    fclose(fptr);
+
+    printf("\nEMPLOYEE DETAILS FROM FILE:\n");
+    for (int i = 0; i < 3; i++) 
+    {
+        printf("\nEmployee %d:\n", i + 1);
+        printf("Name: %s\n", readEmployees[i].name);
+        printf("ID: %d\n", readEmployees[i].id);
+        printf("Salary: %.2f\n", readEmployees[i].salary);
+        printf("City: %s\n", readEmployees[i].address.city);
+        printf("Zip Code: %d\n", readEmployees[i].address.zipcode);
+    }
 }
